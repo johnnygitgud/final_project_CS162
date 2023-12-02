108ca09f9d18e84cbc595aca75943277ae37fd60
JohnnyTamale <foxsauceplanet@protonmail.com>
2023-12-01 19:07:27 -0800

    Inheritance in classes and test file added

--- browse_fileGUI.py
+++ browse_fileGUI.py
@@ -1,67 +1,50 @@
-####This is a sample code from https://www.geeksforgeeks.org/file-explorer-in-python-using-tkinter/
-#The project is not complete. I need an encrytpion algorithm as well classes for different parts.
-#The code so far does simulate file explorer but it only walks you to a file in question. It does not open the file. 
-# Python program to create 
-# a file explorer in Tkinter
-
-# import all components
-# from the tkinter library
 from tkinter import *
-
-# import filedialog module
 from tkinter import filedialog
 
-# Function for opening the 
-# file explorer window
-def browseFiles():
-	filename = filedialog.askopenfilename(initialdir = "/",
-										title = "Select a File",
-										filetypes = (("Text files",
-														"*.txt*"),
-													("all files",
-														"*.*")))
-	
-	# Change label contents
-	label_file_explorer.configure(text="File Opened: "+filename)
-	
-	
-																								
-# Create the root window
-window = Tk()
+class BaseWindow:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('File Explorer')
+        self.root.geometry("500x500")
+        self.root.config(background="white")
 
-# Set window title
-window.title('File Explorer')
+class FileExplorer(BaseWindow):
+    def __init__(self, root):
+        super().__init__(root)
+        self.label_file_explorer = Label(root, text="File Explorer using Tkinter", width=100, height=4, fg="blue")
+        self.label_file_explorer.grid(column=1, row=1)
 
-# Set window size
-window.geometry("500x500")
+        self.button_explore = Button(root, text="Browse Files", command=self.browse_files)
+        self.button_explore.grid(column=1, row=2)
 
-#Set window background color
-window.config(background = "white")
+    def browse_files(self):
+        filename = filedialog.askopenfilename(initialdir="/", title="Select a File",
+                                               filetypes=(("All files", "*.*"),))
+        self.label_file_explorer.configure(text="File Opened: " + filename)
+        # Pass the selected file to the EncryptionHandler for further processing
 
-# Create a File Explorer label
-label_file_explorer = Label(window, 
-							text = "File Explorer using Tkinter",
-							width = 100, height = 4, 
-							fg = "blue")
+class EncryptionHandler:
+    def encrypt_file(self, file_path):
+        # Implement your encryption logic here
+        print(f"Encrypting file: {file_path}")
 
-	
-button_explore = Button(window, 
-						text = "Browse Files",
-						command = browseFiles) 
+        # For illustration, you might want to save the encrypted file
 
-button_exit = Button(window, 
-					text = "Exit",
-					command = exit) 
+class Application(BaseWindow):
+    def __init__(self, root):
+        super().__init__(root)
+        self.file_explorer = FileExplorer(root)
+        self.encryption_handler = EncryptionHandler()
 
-# Grid method is chosen for placing
-# the widgets at respective positions 
-# in a table like structure by
-# specifying rows and columns
-label_file_explorer.grid(column = 1, row = 1)
+        self.encrypt_button = Button(root, text="Encrypt", command=self.handle_encryption)
+        self.encrypt_button.grid(column=1, row=3)
 
-button_explore.grid(column = 1, row = 2)
+    def handle_encryption(self):
+        selected_file = self.file_explorer.label_file_explorer.cget("text").replace("File Opened: ", "")
+        if selected_file:
+            self.encryption_handler.encrypt_file(selected_file)
 
-button_exit.grid(column = 1,row = 3)
-
-# Let the window wait for any events
-window.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    app = Application(root)
+    root.mainloop()
